a78453e62cf41b5b8bca9a751adfc54dad3e0335
Trung Nghia Pham <fb.trungnghia13@gmail.com>
2024-09-23 17:33:23 +0700

    bim ngu

---
+++ application/logs/log-2024-09-23.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-09-23 01:59:44 --> Severity: error --> Exception: No connection could be made because the target machine actively refused it C:\Users\ASUS-PRO\Downloads\Acon v1.9\Acon v1.9\cms\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2024-09-23 02:00:01 --> Severity: error --> Exception: No connection could be made because the target machine actively refused it C:\Users\ASUS-PRO\Downloads\Acon v1.9\Acon v1.9\cms\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2024-09-23 02:00:53 --> Severity: error --> Exception: No connection could be made because the target machine actively refused it C:\Users\ASUS-PRO\Downloads\Acon v1.9\Acon v1.9\cms\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2024-09-23 05:31:42 --> Severity: error --> Exception: No connection could be made because the target machine actively refused it C:\Users\ASUS-PRO\Downloads\Acon v1.9\Acon v1.9\cms\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2024-09-23 05:31:46 --> Severity: error --> Exception: No connection could be made because the target machine actively refused it C:\Users\ASUS-PRO\Downloads\Acon v1.9\Acon v1.9\cms\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2024-09-23 06:13:03 --> Severity: 8192 --> setcookie(): Passing null to parameter #2 ($value) of type string is deprecated C:\Users\ASUS-PRO\Downloads\Acon v1.9\Acon v1.9\cms\system\libraries\Session\Session_driver.php 142
